41430f2742e8f60b6d9a8ba72ae45b8fc7b6c688
andrew <horolskyandrey@gmail.com>
2024-07-14 20:27:42 +0300

    add rescue from NoMethodError with 501

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  rescue_from NoMethodError, with: :not_implemented
+
+  private
+
+  def not_implemented
+    render json: {
+      message: "the method not implemented"
+    },
+      status: 501
+  end
 end
