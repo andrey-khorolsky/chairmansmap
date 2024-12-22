4f3c57c22025f289ad1efae8f25d7137b0952cea
andrew <horolskyandrey@gmail.com>
2024-12-22 14:03:20 +0300

    owner test

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,5 +4,5 @@ class Owner < ApplicationRecord
   has_one :plot_datum, through: :plot
 
   validates :plot_id, :person_id, presence: true
-  validates :active_to, uniqueness: {scope: :plot_id}, if: proc { active_to.nil? }
+  validates :plot_id, uniqueness: {scope: :active_to}, unless: :active_to?
 end
